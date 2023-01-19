fe4489af545e09e1a3c913dd590eaafdce49bbe6
NAN <NAN>
2023-01-19 13:03:38 -0500

    Developer time

--- assembler.c
+++ assembler.c
@@ -3188,7 +3188,7 @@ static void handle_cli_args(int argc, char** argv){
 		){
 			ASSEMBLER_SHOW_HELP:;
 			if(!clear_output) {
-				fputs("Usage: ",stdout);
+				fputs("Usage:  ",stdout);
 				fputs(argv[0],stdout);
 				fputs("[ARGS...]\n",stdout);
 				
