f83b7089420323c6fa583333370323ec43424c83
NAN <NAN>
2023-01-20 08:43:39 -0500

    Developer time

--- s16_1/harden.asm
+++ s16_1/harden.asm
@@ -72,19 +72,104 @@ harden_main:
 		lla %0xDE04%;//code for the exec region syscall.
 		lb 4;		//What region to exec?
 		syscall;	//Make the call
-		
+
+		//first, set up our stack.
+		//get some zeroes in there.
+		astp; popa;
+		la 0; rx0a; rx0push;rx0push;rx0push;
+		astp;popa;
+
+		//Do a far write.
 		sc %0xffFF%;		
-		llb %0xffFF%;		
-		lrx0 %/0xffFFffFF%;
+		lla %0xffFF%;		
+		lrx0 %/0xAABBCCDD%;
 		faristrx0;			
-							//test 2- try to write 2 bytes
-		ab;					
-		faristla;			
+
+		//this should have written 4 FF bytes, one to the last byte of memory,
+		//and three at the start of memory.
+
+		//test if they are where they should be.
+
+
+		farlda %&0xffFFff%
+		lb 0xAA; cmp;
+		jne %harden_fail%
+
+		farlda %&0%
+		lb 0xBB
+		cmp
+		jne %harden_fail%
+
+		farlda %&1%
+		lb 0xCC; cmp;
+		jne %harden_fail%
+
+		farlda %&2%
+		lb 0xDD; cmp;
+		jne %harden_fail%
+
+		//store rx3.
+
+		lrx3 %/0%
+		farstrx3 %&0xffFFffFF%;
+
+		//Test for zero.
+
+
+		farlda %&0xffFFff%
+		lb 0; cmp;
+		jne %harden_fail%
+		
+		farlda %&0%;
+		lb 0; cmp;
+		jne %harden_fail%
+
+		farlda %&1%
+		lb 0; cmp;
+		jne %harden_fail%
+
+		farlda %&2%
+		lb 0; cmp;
+		jne %harden_fail%
+
+		//test 2- write 2 bytes
+		//test 2- try to write 2 bytes
+		lla %0xCADF%;
+		sc %0xffFF%
+		llb %0xffFF%					
+		faristla;
+
+		farlda %&0xffFFff%
+		lb 0xCA; cmp;
+		jne %harden_fail%
+		
+		farlda %&0%;
+		lb 0xDF; cmp;
+		jne %harden_fail%
+
+		lrx0 %/0xBDCDEDFD%
 							//test 3- use literal address
-		farstrx0 %&0xffFFff%;
+		farstrx0 %&0xffFFffFF%;
+
+		farlda %&0xffFFff%
+		lb 0xBD; cmp;
+		jne %harden_fail%
+		
+		farlda %&0%;
+		lb 0xCD; cmp;
+		jne %harden_fail%
+
+		farlda %&1%
+		lb 0xED; cmp;
+		jne %harden_fail%
+
+		farlda %&2%
+		lb 0xFD; cmp;
+		jne %harden_fail%
+		
 		lla %0xDE02%;syscall;
 		sc %harden_whiletop%; jmpc;
-
+	harden_fail:
 		lb 0; div;
 
 
